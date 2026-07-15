54f62d46faac214eb0397ed8de748e65cb8cdabf
akram09 <akram.boutouchent@canonical.com>
2026-07-15 10:14:16 +0200

    patch: Fix snap linting and use ubuntu-26 runners

--- opensearch/snaps/charmed/scripts/helpers/conf-setter.py
+++ opensearch/snaps/charmed/scripts/helpers/conf-setter.py
@@ -8,15 +8,14 @@ from enum import Enum
 
 from ruamel.yaml import YAML, CommentedSeq
 
-
 """Utilities for editing yaml configuration files at any level of nestedness while maintaining comments"""
 
 
 class OutputType(Enum):
-    file = 'file'
-    obj = 'obj'
-    console = 'console'
-    all = 'all'
+    file = "file"
+    obj = "obj"
+    console = "console"
+    all = "all"
 
     def __str__(self):
         return self.value
@@ -28,16 +27,23 @@ class ConfigSetter:
         self.yaml = YAML()
 
     def load(self, config_file: str) -> dict[str, any]:
-        with open(config_file, mode='r') as f:
+        with open(config_file, mode="r") as f:
             data = self.yaml.load(f)
 
         return data
 
-    def put(self, config_file: str, key_path: str, val: any, sep='/',
-            output_type: OutputType = OutputType.file, inline_array: bool = False,
-            output_file: str = None) -> dict[str, any]:
+    def put(
+        self,
+        config_file: str,
+        key_path: str,
+        val: any,
+        sep="/",
+        output_type: OutputType = OutputType.file,
+        inline_array: bool = False,
+        output_file: str = None,
+    ) -> dict[str, any]:
 
-        with open(config_file, mode='r') as f:
+        with open(config_file, mode="r") as f:
             data = self.yaml.load(f)
 
         self.__deep_update(data, key_path.split(sep), val)
@@ -45,24 +51,29 @@ class ConfigSetter:
         if inline_array:
             data = self.__inline_array_format(data, key_path.split(sep), val)
 
-        self.__dump(data,
-                    output_type,
-                    config_file if output_file is None else output_file)
+        self.__dump(
+            data, output_type, config_file if output_file is None else output_file
+        )
 
         return data
 
-    def delete(self, config_file: str, key_path: str, sep='/',
-               output_type: OutputType = OutputType.file,
-               output_file: str = None) -> dict[str, any]:
+    def delete(
+        self,
+        config_file: str,
+        key_path: str,
+        sep="/",
+        output_type: OutputType = OutputType.file,
+        output_file: str = None,
+    ) -> dict[str, any]:
 
-        with open(config_file, mode='r') as f:
+        with open(config_file, mode="r") as f:
             data = self.yaml.load(f)
 
         self.__deep_delete(data, key_path.split(sep))
 
-        self.__dump(data,
-                    output_type,
-                    config_file if output_file is None else output_file)
+        self.__dump(
+            data, output_type, config_file if output_file is None else output_file
+        )
 
         return data
 
@@ -71,7 +82,7 @@ class ConfigSetter:
             self.yaml.dump(data, sys.stdout)
 
         if output_type in [OutputType.file, OutputType.all]:
-            with open(target_file, mode='w') as f:
+            with open(target_file, mode="w") as f:
                 self.yaml.dump(data, f)
 
     def __deep_update(self, source, node_keys: list[str], val: any):
@@ -79,7 +90,7 @@ class ConfigSetter:
             return val
 
         if source is None:
-            if node_keys[0].startswith('['):
+            if node_keys[0].startswith("["):
                 source = []
             else:
                 source = {}
@@ -98,7 +109,9 @@ class ConfigSetter:
             if target_index == -1:
                 source.append(self.__deep_update(None, node_keys, val))
             else:
-                source[target_index] = self.__deep_update(source[target_index], node_keys, val)
+                source[target_index] = self.__deep_update(
+                    source[target_index], node_keys, val
+                )
 
             return source
 
@@ -114,7 +127,9 @@ class ConfigSetter:
         leaf_level = self.__leaf_level(source, node_keys)
         leaf_key = node_keys.pop(0)
 
-        if leaf_key in leaf_level and (isinstance(leaf_level[leaf_key], Mapping) or not leaf_key.startswith('[')):
+        if leaf_key in leaf_level and (
+            isinstance(leaf_level[leaf_key], Mapping) or not leaf_key.startswith("[")
+        ):
             del leaf_level[leaf_key]
             return
 
@@ -127,7 +142,7 @@ class ConfigSetter:
             return current
 
         current_key = node_names.pop(0)
-        if current_key.startswith('[') and current_key.endswith(']'):
+        if current_key.startswith("[") and current_key.endswith("]"):
             target_index = self.__target_array_index(current, current_key)
             return self.__leaf_level(current[target_index], node_names)
 
@@ -141,7 +156,7 @@ class ConfigSetter:
 
         # where user provides a key [key:val] or [key]
         if str_index and not str_index.isnumeric():
-            key = str_index.split(':')
+            key = str_index.split(":")
             if len(key) == 1:
                 index = source.index(str_index)
             else:
@@ -152,7 +167,7 @@ class ConfigSetter:
                         break
 
                 if index == -1:
-                    raise ValueError(f'{str_index} not found in the object.')
+                    raise ValueError(f"{str_index} not found in the object.")
 
         exists = index is not None or (str_index and (int(str_index) < len(source)))
         if not exists:
@@ -160,7 +175,9 @@ class ConfigSetter:
 
         return int(str_index) if index is None else index
 
-    def __inline_array_format(self, data, node_keys: list[str], val: list[any]) -> dict[str, any]:
+    def __inline_array_format(
+        self, data, node_keys: list[str], val: list[any]
+    ) -> dict[str, any]:
         leaf_k = node_keys[-1]
 
         leaf_l = self.__leaf_level(data, node_keys)
@@ -183,63 +200,82 @@ def parse_args():
         if len(clean_input) == 1:
             return clean_input
 
-        raise ValueError('Expected single character.')
+        raise ValueError("Expected single character.")
 
     def __body_value_arg(input_val):
         if input_val.isnumeric():
             return int(input_val)
 
-        if input_val.startswith('{') and input_val.endswith('}'):
+        if input_val.startswith("{") and input_val.endswith("}"):
             return json.loads(input_val)
 
-        if input_val.startswith('[') and input_val.endswith(']'):
+        if input_val.startswith("[") and input_val.endswith("]"):
             return ast.literal_eval(input_val)
 
         return input_val
 
     def __parse():
-        parser = ArgumentParser(description='Utility script for editing YAML conf files.')
+        parser = ArgumentParser(
+            description="Utility script for editing YAML conf files."
+        )
 
-        parser.add_argument('-f', '--file',
-                            type=str,
-                            required=True,
-                            help='Path of the source/target YAML file.')
+        parser.add_argument(
+            "-f",
+            "--file",
+            type=str,
+            required=True,
+            help="Path of the source/target YAML file.",
+        )
 
-        parser.add_argument('-k', '--key',
-                            type=str,
-                            required=True,
-                            help='Key of the element to target.')
+        parser.add_argument(
+            "-k", "--key", type=str, required=True, help="Key of the element to target."
+        )
 
-        parser.add_argument('-v', '--value',
-                            type=__body_value_arg,
-                            required=False,
-                            help='New value to set - for complex data types, this should be formatted as list / dict.')
+        parser.add_argument(
+            "-v",
+            "--value",
+            type=__body_value_arg,
+            required=False,
+            help="New value to set - for complex data types, this should be formatted as list / dict.",
+        )
 
-        parser.add_argument('-i', '--inline-array',
-                            type=bool,
-                            required=False,
-                            default=False,
-                            help='If value is an array of simple values (str, int etc.), whether to write it between '
-                                 'square brackets on the same line or as a multiline yaml list.')
+        parser.add_argument(
+            "-i",
+            "--inline-array",
+            type=bool,
+            required=False,
+            default=False,
+            help="If value is an array of simple values (str, int etc.), whether to write it between "
+            "square brackets on the same line or as a multiline yaml list.",
+        )
 
-        parser.add_argument('-s', '--sep',
-                            type=__single_char_arg,
-                            default='/',
-                            required=False,
-                            help='Separator character for traversal.')
+        parser.add_argument(
+            "-s",
+            "--sep",
+            type=__single_char_arg,
+            default="/",
+            required=False,
+            help="Separator character for traversal.",
+        )
 
-        parser.add_argument('-o', '--out',
-                            type=OutputType,
-                            choices=list(OutputType),
-                            default=OutputType.all,
-                            required=False,
-                            help='Output type of the transformation.')
+        parser.add_argument(
+            "-o",
+            "--out",
+            type=OutputType,
+            choices=list(OutputType),
+            default=OutputType.all,
+            required=False,
+            help="Output type of the transformation.",
+        )
 
-        parser.add_argument('-d', '--delete',
-                            type=bool,
-                            default=False,
-                            required=False,
-                            help='Delete node (Not implemented).')
+        parser.add_argument(
+            "-d",
+            "--delete",
+            type=bool,
+            default=False,
+            required=False,
+            help="Delete node (Not implemented).",
+        )
 
         return parser.parse_args()
 
@@ -251,12 +287,16 @@ if __name__ == "__main__":
 
     conf_setter = ConfigSetter()
     if args.delete:
-        conf_setter.delete(args.file, args.key,
-                           sep=args.sep, output_type=args.out)
+        conf_setter.delete(args.file, args.key, sep=args.sep, output_type=args.out)
     else:
-        conf_setter.put(args.file, args.key, args.value,
-                        sep=args.sep, output_type=args.out,
-                        inline_array=args.inline_array)
+        conf_setter.put(
+            args.file,
+            args.key,
+            args.value,
+            sep=args.sep,
+            output_type=args.out,
+            inline_array=args.inline_array,
+        )
 
     # path = 'opensearch_put.yml'
     # put(path, 'cluster.name', 'new_name')
